6c9a30ac6d893c5f26b98040b5ec68d4b1b19253
liuxufeng <zxc5050@qq.com>
2021-04-14 16:55:19 +0800

    开始学习

--- src/main/java/com/example/netty/test.java
+++ src/main/java/com/example/netty/test.java
@@ -1,7 +1,33 @@
 package com.example.netty;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class test {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
+        System.out.println("Hello world!");
+
+//        List<String> list = new ArrayList<>();
+//        list.add("1");
+//        list.add("2");
+//        list.add("3");
+//        List<String> result = list.stream().filter(item->false).collect(Collectors.toList());
+//        System.out.println(result.size());
+//        System.out.println(result);
+
+        List<BigDecimal> list = new ArrayList<>();
+        list.add(new BigDecimal("1"));
+        list.add(new BigDecimal("2"));
+        list.add(new BigDecimal("3"));
+        System.out.println(list.size());
+        System.out.println(list);
+        List<BigDecimal> result = list.stream().filter(item->false).collect(Collectors.toList());
+        System.out.println(result.size());
+        System.out.println(result);
+        BigDecimal totalValue = result.stream().
+                reduce(new BigDecimal(0), BigDecimal::add);
+        System.out.println(totalValue);
     }
 }
