1e97741ccfceb482de77e3d35f8741aacc381ee0
liuxufeng <zxc5050@qq.com>
2021-04-16 01:33:06 +0800

    class 1

--- src/test/java/com/example/netty/c1/TestByteBuffer.java
+++ src/test/java/com/example/netty/c1/TestByteBuffer.java
@@ -1,10 +1,13 @@
 package com.example.netty.c1;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
+@Slf4j
 public class TestByteBuffer {
     public static void main(String[] args) {
         //FileChannel
@@ -12,13 +15,20 @@ public class TestByteBuffer {
         try(FileChannel channel = new FileInputStream("data.txt").getChannel()){
             // 准备缓冲区
             ByteBuffer buffer = ByteBuffer.allocate(10);
-            // 从 channel 读取数据，向 buffer 写入
-            channel.read(buffer);
-            // 打印 buffer 的内容
-            buffer.flip(); //切换至读模式
-            while (buffer.hasRemaining()){// 是否还有剩余未读数据
-                byte b = buffer.get();
-                System.out.println((char) b);
+            while (true){
+                // 从 channel 读取数据，向 buffer 写入
+                int len = channel.read(buffer);
+                log.debug("从缓冲区读到了几个字节：{}", len);
+                if(len == -1){ //没有内容了
+                    break;
+                }
+                // 打印 buffer 的内容
+                buffer.flip(); //切换到读模式
+                while (buffer.hasRemaining()){// 是否还有剩余未读数据
+                    byte b = buffer.get();
+                    log.debug("读取到的字节 {}", (char)b);
+                }
+                buffer.clear(); //切换到写模式
             }
         }catch (IOException e){
             e.printStackTrace();
