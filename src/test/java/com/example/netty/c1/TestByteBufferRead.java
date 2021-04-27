547197e2ab2e1879036f8f23c905597a5e6ee6e2
liuxufeng <zxc5050@qq.com>
2021-04-27 16:51:46 +0800

    class 1

--- src/test/java/com/example/netty/c1/TestByteBufferRead.java
+++ src/test/java/com/example/netty/c1/TestByteBufferRead.java
@@ -2,11 +2,18 @@ package com.example.netty.c1;
 
 import java.nio.ByteBuffer;
 
+import static com.example.netty.c1.ByteBufferUtil.debugAll;
+
 public class TestByteBufferRead {
 
     public static void main(String[] args) {
         ByteBuffer buffer = ByteBuffer.allocate(10);
         buffer.put(new byte[]{'a','b','c','d'});
-        buffer.flip();//切换至模式
+
+        //切换至读模式
+        buffer.flip();
+
+        buffer.get(new byte[4]);
+        debugAll(buffer);
     }
 }
--- src/test/java/com/example/netty/c1/TestByteBufferReadWrite.java
+++ src/test/java/com/example/netty/c1/TestByteBufferReadWrite.java
@@ -2,19 +2,21 @@ package com.example.netty.c1;
 
 import java.nio.ByteBuffer;
 
+import static com.example.netty.c1.ByteBufferUtil.debugAll;
+
 public class TestByteBufferReadWrite {
     public static void main(String[] args) {
         ByteBuffer buffer = ByteBuffer.allocate(10);
         buffer.put((byte) 0x61); //'a'
-        ByteBufferUtil.debugAll(buffer);
+        debugAll(buffer);
         buffer.put(new byte[]{0x62, 0x63, 0x64});// b c d
-        ByteBufferUtil.debugAll(buffer);
+        debugAll(buffer);
         buffer.flip();
         System.out.println(buffer.get());
-        ByteBufferUtil.debugAll(buffer);
+        debugAll(buffer);
         buffer.compact();
-        ByteBufferUtil.debugAll(buffer);
+        debugAll(buffer);
         buffer.put(new byte[]{0x65, 0x6f});
-        ByteBufferUtil.debugAll(buffer);
+        debugAll(buffer);
     }
 }
