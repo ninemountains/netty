2fa3896c635ccf8fc528938149e2e8aca22e237e
liuxufeng <zxc5050@qq.com>
2021-04-28 14:46:39 +0800

    class 1

---
+++ src/test/java/com/example/netty/c1/TestByteBufferString.java
@@ -0,0 +1,28 @@
+package com.example.netty.c1;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static com.example.netty.c1.ByteBufferUtil.debugAll;
+
+public class TestByteBufferString {
+    public static void main(String[] args) {
+        //1. 字符串转为ByteBuffer
+
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+        buffer.put("hello".getBytes());
+        debugAll(buffer);
+
+        //2. Charset
+        ByteBuffer buffer2 = StandardCharsets.UTF_8.encode("hello");
+        debugAll(buffer2);
+
+        //3. wrap
+        ByteBuffer buffer3 = ByteBuffer.wrap("hello".getBytes());
+        debugAll(buffer3);
+
+        //
+        String str1 = StandardCharsets.UTF_8.decode(buffer2).toString();
+        System.out.println(str1);
+    }
+}
